fb163ba0ec47a1a378ad25588fe6bbfaa02fd6e9
Kya Gaming <kyakarnahai21@gmail.com>
2025-08-17 05:28:51 +0000

    Set up Firebase Backend

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id"
-};
+    apiKey: "AIzaSyDTWWMd5p3FUYpoOOsXpQZ5GFmOfV7WqwQ",
+    authDomain: "hospital-management-7d105.firebaseapp.com",
+    projectId: "hospital-management-7d105",
+    storageBucket: "hospital-management-7d105.firebasestorage.app",
+    messagingSenderId: "53540545971",
+    appId: "1:53540545971:web:72d8c7ada6257169f857c6",
+    measurementId: "G-GHPMMYENV1"
+  };
+  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
